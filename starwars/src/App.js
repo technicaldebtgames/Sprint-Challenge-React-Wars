adb337bbd8cd182bbb1b63545cb96fb120e4b940
Dan Brioli <technicaldebtgames@gmail.com>
2020-05-15 11:32:57 -0400

    init

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from "axios";
+import {API_URL, API_KEY, API_DATE_TAG} from "../constants.js"
 import './App.css';
 
 const App = () => {
@@ -8,6 +10,26 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  // Build the api url string
+  let apiString = `${API_URL}${API_KEY}`;
+
+  if (props.useCustomDate) {
+      apiString += `${API_DATE_TAG}${props.customDate}`;
+  }
+  // done building api string
+
+  // use api string to get data
+  axios.get(`${apiString}`)
+  .then(result => {
+    console.log(result);
+  })
+  .catch (error => {
+    console.log("Error fetching data from API.");
+  })
+  .finally ( () => {
+    console.log("API call has finished.");
+  });
 
   return (
     <div className="App">
---
+++ starwars/src/constants.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://rickandmortyapi.com/api/';
+export const CHAR_TAG = 'character/';
+export const LOC_TAG = 'location/';
+export const EP_TAG = 'episode/';
