58fffdcc910c8b19aff2088bcf8508b6bfcd1cd5
Dan Brioli <technicaldebtgames@gmail.com>
2020-05-15 12:37:41 -0400

    some infinite api call error

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import axios from "axios";
-import {API_URL, API_KEY, API_DATE_TAG} from "../constants.js"
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import Character from './components/Character.js'
+import {CHAR_TAG, EP_TAG, LOC_TAG, API_URL} from "./constants.js"
 import './App.css';
+
+const AppContainer = styled.div`
+
+`;
+
+const AppTitle = styled.div`
+
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,31 +21,41 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  // Build the api url string
-  let apiString = `${API_URL}${API_KEY}`;
+  let [characters, setCharacters] = useState([]);
 
-  if (props.useCustomDate) {
-      apiString += `${API_DATE_TAG}${props.customDate}`;
-  }
-  // done building api string
+  useEffect(() => {
 
-  // use api string to get data
-  axios.get(`${apiString}`)
-  .then(result => {
-    console.log(result);
-  })
-  .catch (error => {
-    console.log("Error fetching data from API.");
-  })
-  .finally ( () => {
-    console.log("API call has finished.");
+    // Build the api url string
+    let apiString = `${API_URL}${CHAR_TAG}`;
+    // done building api string
+  
+    // use api string to get data
+    axios.get(`${apiString}`)
+    .then(result => {
+      console.log(result);
+      setCharacters(result.data.results);
+      console.log(characters);
+    })
+    .catch (error => {
+      console.log("Error fetching data from API.");
+    })
+    .finally ( () => {
+      console.log("API call has finished.");
+    });
+
   });
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(characterData => {
+        return <Character key={characterData.id} character={characterData}/>
+        console.log("passing these as key and character to props:");
+        console.log(characterData.id);
+        console.log(characterData);
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,61 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+//div container
+    // img image
+    // div innerContainer
+        // h2 name
+        // h3 status
+        // h3 species
+        // h3 origin.name
+        // h3 location.name
+
+const CharacterContainer = styled.div`
+
+`;
+
+const CharacterImage = styled.img`
+
+`;
+
+const CharacterInnerContainer = styled.div`
+
+`;
+
+const CharacterName = styled.h2`
+
+`;
+
+const CharacterStatus = styled.h3`
+
+`;
+
+const CharacterSpecies = styled.h3`
+
+`;
+
+const CharacterOrigin = styled.h3`
+
+`;
+
+const CharacterLocation = styled.h3`
+
+`;
+
+export default function Character(props) {
+
+    return (
+        <CharacterContainer>
+            <CharacterImage src={props.character.image}></CharacterImage>
+            <CharacterInnerContainer>
+                <CharacterName>{props.character.name}</CharacterName>
+                <CharacterStatus>{props.character.status}</CharacterStatus>
+                <CharacterSpecies>{props.character.species}</CharacterSpecies>
+                <CharacterOrigin>{props.character.origin.name}</CharacterOrigin>
+                <CharacterLocation>{props.character.location.name}</CharacterLocation>
+            </CharacterInnerContainer>
+        </CharacterContainer>
+    );  
+
+}
